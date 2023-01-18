b3cd93177bfa2748fd47d5d3cab07385dc85d2a8
daf1na <dafinabalaj345@gmail.com>
2023-01-19 00:10:35 +0100

    javascript dropdown

--- akomodimi.js
+++ akomodimi.js
@@ -1,3 +1,21 @@
-function pageRedirect() {
-    window.location.href = "file:///C:/Users/Dafina%20Balaj/Documents/GitHub/Projekti_Web/peja%20prizren%20gjakova.html";
-  }      
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
