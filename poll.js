d6ca3c086bec5853a7c0520180da97f80c45ec9a
d3shira <shirarandobrava@live.com>
2023-01-19 02:02:21 +0100

    eh

--- poll.js
+++ poll.js
@@ -1,4 +1,6 @@
 let poll={
     question: "Which one of these food would you like to try the most?", 
-    answers:["","","","","","","","","",""]
+    answers:["","","","","","","","","",""],
+    pollCount:100,
+    
 }
