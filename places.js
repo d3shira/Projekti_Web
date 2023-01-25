29f80f68c7dcd1e18fb7320f90293afc8aa6dbf2
dafiinaa <dafinaasadiku@gmail.com>
2023-01-25 12:45:09 +0100

    ndryshime

--- places.js
+++ places.js
@@ -22,5 +22,13 @@ for(const div of divs){
     div.addEventListener("click", myFunction);
 }
 
+var today = new Date();
+var day = today.getDay();
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = "Date: " + date+ '<br>' +"Time: "+time;
+ 
+document.getElementById("displayDateTime").innerHTML = dateTime + ' <br> Day: ' + daylist[day];
 
 
