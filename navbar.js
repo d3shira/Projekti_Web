c736aad5082bea4e2555282e5cc5beade61bb5ea
Besmira75 <besmira.berisha129@gmail.com>
2023-01-30 21:16:56 +0000

    .

--- navbar.js
+++
@@ -1,9 +0,0 @@
-const  header = document.querySelector('.navbari')
-fetch('/navbar.html')
-.then(res=>res.text())
-.then(data=>{
-    header.innerHTML=data
-    const parser = new DOMParser()
-    const doc =parser.parseFromString(data, 'text/html')
-    eval(doc.querySelector('script').textContent)
-})
